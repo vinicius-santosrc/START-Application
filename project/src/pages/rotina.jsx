bffc5cae55d61bc37a0a08b38f18407ad1edda63
Vinícius Santos <viniciusssantos.pa@gmail.com>
2023-08-11 02:03:58 -0300

    .

--- project/src/pages/rotina.jsx
+++ project/src/pages/rotina.jsx
@@ -2,11 +2,10 @@ import React, { createElement } from 'react';
 import HeaderApp from '../components/HeaderApp';
 import { app, auth } from '../systemlogin';
 import { json } from 'react-router-dom';
+import Swal from 'sweetalert2'
 
 
-function organizarrotina() {
-    alert('button organize sua rotina')
-}
+
 
 function buttonrotinaauto() {
     alert('button rotina auto')
@@ -25,9 +24,13 @@ function adicionarotinasexistentes(u) {
     .orderBy('date', 'desc')
     .get()
     .then(s => {
-        const rotinas = s.docs.map(doc => doc.data())
+        const rotinas = s.docs.map(doc => ({
+            ...doc.data(),
+            uid: doc.id
+            }));
         if(rotinas) {
             showrotinas(rotinas)
+
         }
         if(rotinas.length == 0) {
             document.querySelector('.rotinasmobile').style.display = 'none';document.querySelector('.rotinas-pc').style.display = 'none'
@@ -84,19 +87,332 @@ function showrotinas(rotinas) {
 
 
         function loadrotinas() {
-            let dr = document.createElement('div')
-
-            dr.setAttribute('class', 'rotina_item')
-            dr.innerHTML = JSON.stringify(rotinas.rotina)
-            insiderotinalist.appendChild(dr)
+            window.location.href = './rotina?uid=' + rotinas.uid
             
         }
     })
 
 }
 
+getUrlRotina()
+
+
+function getUrlRotina() {
+    const urlParams = new URLSearchParams(window.location.search)
+    return urlParams.get('uid')
+}
+
+function isnewrotine() {
+    return getUrlRotina() ? false : true;
+}
+
+function excluirrotina() {
+    
+    const uid = getUrlRotina();
+    app.firestore()
+    .collection("rotinas")
+    .doc(uid)
+    .delete()
+    .then(() => {
+        Swal.fire(
+            'Deletada!',
+            'Sua rotina foi excluída com sucesso.',
+            'success'
+          )
+        window.location.href='./rotina'
+    })
+    .catch((e) => {
+        alert('ERRO: ' + e)
+        
+        window.location.href='./rotina'
+    })
+}
+if (!isnewrotine()) {
+    const uid = getUrlRotina();
+    app.firestore()
+    .collection("rotinas")
+    .doc(uid)
+    .get()
+    .then(doc => {
+        if(doc.exists) {
+            document.querySelector('.rotinaedit').style.display = 'block'
+            var rotinaatual = doc.data()
+            document.querySelector('.nameeditrotina').value = rotinaatual.name
+            document.querySelector('.desceditrotina').value = rotinaatual.description
+        }
+        else {
+            window.location.href="./rotina"
+        }
+    })
+    .catch((e) => {
+        alert('ERRO: ' + e)
+        window.location.href="./rotina"
+    })
+}
+
 function formatDate(date) {
     return new Date(date).toLocaleDateString('pt-br')
+}
+
+function saverotina() {
+    const data = new Date()
+    const year = data.getFullYear()
+    const mounth = data.getMonth() + 1
+    const day = data.getDate()
+        return {
+            date: `${year}-${mounth}-${day}`,
+            name: document.querySelector('.nameeditrotina').value,
+            description: document.querySelector('.desceditrotina').value,
+            rotina: '',
+            user: {
+                uid: app.auth().currentUser.uid
+            }
+        }
+}
+
+function salvarRotina() {
+    const updatevar = saverotina();
+
+    update(updatevar)
+
+
+    
+}
+
+function update(updatevar) {
+    app.firestore()
+    .collection("rotinas")
+    .doc(getUrlRotina())
+    .update(updatevar)
+    .then(() => {
+        Swal.fire(
+            'Sucesso!',
+            'Você salvou sua rotina com sucesso.',
+            'success'
+          )
+    })
+    .catch((e) => {
+        alert('ERRO: ' + e)
+    })
+}
+
+function fechareditrotina() {
+    
+    window.location.href = './rotina'
+}
+
+function showcomponentadd() {
+
+}
+
+function verifystep() {
+    if(step == 1) {
+        document.querySelector('.etapa1').style.display = 'block'
+        document.querySelector('.etapa2').style.display = 'none'
+        document.querySelector('.etapa3').style.display = 'none'
+        document.querySelector('.etapa4').style.display = 'none'
+        document.querySelector('.stepbutton').style.display = 'block'
+        document.querySelector('.stepanme').innerHTML = 'Etapa 1'
+        document.querySelector('.createrotina').style.display = 'none'
+        
+    }
+    else if (step == 2) {
+        document.querySelector('.etapa1').style.display = 'none'
+        document.querySelector('.etapa2').style.display = 'block'
+        document.querySelector('.etapa3').style.display = 'none'
+        document.querySelector('.etapa4').style.display = 'none'
+        document.querySelector('.stepbutton').style.display = 'block'
+        document.querySelector('.stepanme').innerHTML = 'Etapa 2'
+        document.querySelector('.createrotina').style.display = 'none'
+
+    }
+    else if(step == 3) {
+        document.querySelector('.etapa1').style.display = 'none'
+        document.querySelector('.etapa2').style.display = 'none'
+        document.querySelector('.etapa3').style.display = 'block'
+        document.querySelector('.etapa4').style.display = 'none'
+        document.querySelector('.stepbutton').style.display = 'block'
+        document.querySelector('.stepanme').innerHTML = 'Etapa 3'
+        document.querySelector('.createrotina').style.display = 'none'
+
+    }
+    else if(step == 4) {
+        document.querySelector('.etapa1').style.display = 'none'
+        document.querySelector('.etapa2').style.display = 'none'
+        document.querySelector('.etapa3').style.display = 'none'
+        document.querySelector('.etapa4').style.display = 'block'
+        document.querySelector('.stepbutton').style.display = 'none'
+        document.querySelector('.stepanme').innerHTML = 'Etapa 4'
+        document.querySelector('.createrotina').style.display = 'block'
+    }
+}
+
+function organizarrotina() {
+    document.querySelector('.organizarsuarotina-tab').style.display = 'block'
+    document.querySelector('.background').style.display = 'block'
+}
+
+function fecharorganizarrotina() {
+    document.querySelector('.organizarsuarotina-tab').style.display = 'none'
+    document.querySelector('.background').style.display = 'none'
+
+    document.querySelector('#namerotine').value = ''
+    document.querySelector('#descrotina').value = ''
+
+    step = 1
+
+    verifystep()
+
+
+}
+
+function createnewrotine() {
+    const namerotine = document.querySelector('#namerotine')
+    const descrotine = document.querySelector('#descrotina')
+
+    if(namerotine.value == '' || descrotine.value == '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Insira todos os dados.',
+          })
+    }
+    
+    else {
+        const data = new Date()
+        const year = data.getFullYear()
+        const mounth = data.getMonth() + 1
+        const day = data.getDate()
+        auth.onAuthStateChanged((u) => {
+            let uidvar = u.uid
+            const newrotine = {
+                date: `${year}-${mounth}-${day}`,
+                description: descrotine.value,
+                name: namerotine.value,
+                rotina: '[]',
+                user: {
+                    uid: uidvar
+                }
+        }
+        app.firestore()
+        .collection('rotinas')
+        .add(newrotine)
+        .then(() => {
+            Swal.fire(
+                'Sucesso!',
+                'Sua rotina foi criada.',
+                'success'
+              )
+            window.location.reload()
+        }).catch((e) => {
+            alert('ERRO: ' + e)
+        })
+        })
+    }
+}
+
+let step = 1
+
+function backbutton() {
+    if(step < 5 && step > 1) {
+        step -= 1
+    }
+    else if(step <= 1) {
+
+    }
+
+    if(step == 1) {
+        document.querySelector('.etapa1').style.display = 'block'
+        document.querySelector('.etapa2').style.display = 'none'
+        document.querySelector('.etapa3').style.display = 'none'
+        document.querySelector('.etapa4').style.display = 'none'
+        document.querySelector('.stepbutton').style.display = 'block'
+        document.querySelector('.stepanme').innerHTML = 'Etapa 1'
+        document.querySelector('.createrotina').style.display = 'none'
+        
+    }
+    else if (step == 2) {
+        document.querySelector('.etapa1').style.display = 'none'
+        document.querySelector('.etapa2').style.display = 'block'
+        document.querySelector('.etapa3').style.display = 'none'
+        document.querySelector('.etapa4').style.display = 'none'
+        document.querySelector('.stepbutton').style.display = 'block'
+        document.querySelector('.stepanme').innerHTML = 'Etapa 2'
+        document.querySelector('.createrotina').style.display = 'none'
+
+    }
+    else if(step == 3) {
+        document.querySelector('.etapa1').style.display = 'none'
+        document.querySelector('.etapa2').style.display = 'none'
+        document.querySelector('.etapa3').style.display = 'block'
+        document.querySelector('.etapa4').style.display = 'none'
+        document.querySelector('.stepbutton').style.display = 'block'
+        document.querySelector('.stepanme').innerHTML = 'Etapa 3'
+        document.querySelector('.createrotina').style.display = 'none'
+
+    }
+    else if(step == 4) {
+        document.querySelector('.etapa1').style.display = 'none'
+        document.querySelector('.etapa2').style.display = 'none'
+        document.querySelector('.etapa3').style.display = 'none'
+        document.querySelector('.etapa4').style.display = 'block'
+        document.querySelector('.stepbutton').style.display = 'none'
+        document.querySelector('.stepanme').innerHTML = 'Etapa 4'
+        document.querySelector('.createrotina').style.display = 'block'
+    }
+}
+
+function nextstep() {
+    if(step < 4) {
+        step += 1
+    }
+    else if(step == 4) {
+
+    }
+
+    if(step == 1) {
+        document.querySelector('.etapa1').style.display = 'block'
+        document.querySelector('.etapa2').style.display = 'none'
+        document.querySelector('.etapa3').style.display = 'none'
+        document.querySelector('.etapa4').style.display = 'none'
+        document.querySelector('.stepbutton').style.display = 'block'
+        document.querySelector('.stepanme').innerHTML = 'Etapa 1'
+        document.querySelector('.createrotina').style.display = 'none'
+        
+    }
+    else if (step == 2) {
+        document.querySelector('.etapa1').style.display = 'none'
+        document.querySelector('.etapa2').style.display = 'block'
+        document.querySelector('.etapa3').style.display = 'none'
+        document.querySelector('.etapa4').style.display = 'none'
+        document.querySelector('.stepbutton').style.display = 'block'
+        document.querySelector('.stepanme').innerHTML = 'Etapa 2'
+        document.querySelector('.createrotina').style.display = 'none'
+
+    }
+    else if(step == 3) {
+        document.querySelector('.etapa1').style.display = 'none'
+        document.querySelector('.etapa2').style.display = 'none'
+        document.querySelector('.etapa3').style.display = 'block'
+        document.querySelector('.etapa4').style.display = 'none'
+        document.querySelector('.stepbutton').style.display = 'block'
+        document.querySelector('.stepanme').innerHTML = 'Etapa 3'
+        document.querySelector('.createrotina').style.display = 'none'
+
+    }
+    else if(step == 4) {
+        document.querySelector('.etapa1').style.display = 'none'
+        document.querySelector('.etapa2').style.display = 'none'
+        document.querySelector('.etapa3').style.display = 'none'
+        document.querySelector('.etapa4').style.display = 'block'
+        document.querySelector('.stepbutton').style.display = 'none'
+        document.querySelector('.stepanme').innerHTML = 'Etapa 4'
+        document.querySelector('.createrotina').style.display = 'block'
+    }
+    
+        
+
+
 }
 
 function RotinaPage() {
@@ -104,6 +420,74 @@ function RotinaPage() {
         <div className="App">
             <HeaderApp />
             <div className='contents-page'>
+                <div className="background" onClick={fecharorganizarrotina}></div>
+                <div className='rotinaedit'>
+                    <div className='rotinaedit-header'>
+                    
+                        <h1>EDITOR DE ROTINAS</h1>
+                        <i onClick={fechareditrotina} className="fa-solid fa-xmark"></i>
+                    </div>
+                    <p>Edite sua rotina existente</p>
+                       <div className='infoofeditrotina'>
+                            <div className='itemofrotina'>
+                                <label>Nome da Rotina: </label>
+                                <input className='nameeditrotina' placeholder='' /> <br />
+                            </div>
+                            <div className='itemofrotina'>
+                            <label>Descrição: </label>
+                            <input className='desceditrotina' placeholder='' /><br />
+                            </div>
+                            <label>Rotina</label>
+                       </div>
+                    
+                        <button onClick={excluirrotina}>EXCLUIR ROTINA</button>
+                        <button onClick={salvarRotina}>SALVAR</button>
+                </div>
+                <div className='organizarsuarotina-tab'>
+                <div className='organizarrotina-top'>
+                    <h1>ORGANIZE SUA ROTINA</h1>
+                    <i onClick={fecharorganizarrotina} className="fa-solid fa-xmark"></i>
+                    </div>
+                <p>Crie sua rotina de acordo com as importâncias de cada tarefa.</p>
+                <div className='organizarrotina-buttons'>
+                   <div className='flexboxrotina'>
+                    <h1 className='stepanme'>ETAPA {step}</h1>
+                    <div className="etapa1">
+                            <label>Digite um nome para sua rotina</label><br />
+                            <input type="text" id="namerotine" placeholder='Nome' />
+                        </div>
+                        <div className="etapa2">
+                            <label>Descrição</label><br />
+                            <input type="text" id="descrotina" placeholder='Exemplo: Foco nos estudos' />
+                        </div>
+                        <div className="etapa3">
+                            <label>Importância</label><br />
+                            <select name="importancia" id="importancia" disabled>
+                                <option value="Baixa">Baixa</option>
+                                <option value="Média">Média</option>
+                                <option value="Alta">Alta</option>
+                            </select>
+                        </div>
+                        <div className="etapa4">
+                            <label>Rotina</label><br />
+                            <button>ADICIONAR COMPONENTE</button>
+                        </div>
+                        <div className='componentadd-organizerotina'>
+                            <p>Nome do componente: </p><input type="text" placeholder='Nome' id="namecomp" />
+                            <div>
+                                <input id="startcomp" placeholder='Início (Ex: 16)' />
+                                <input id="endcomp" placeholder='Término (Ex: 18)' />
+                            </div>
+                            <div>ADICIONAR</div>
+                        </div>
+                   </div>
+                </div>
+                <div className='prevnextbutton'>
+                    <div className='backbutton'><button onClick={backbutton}><i className="fa-solid fa-caret-left"></i> VOLTAR</button></div>
+                    <div className='stepbutton'><button onClick={nextstep}>PRÓXIMO <i className="fa-solid fa-caret-right"></i></button></div>
+                </div>
+                <div className='createrotina'><button onClick={createnewrotine}>Criar rotina</button></div>
+                </div>
                 <div className='background-page'>
                     <img src="./imgs/rotina-page-banner.webp" alt='fundo academia'/>
                 </div>
